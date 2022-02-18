90ccb02ce12c431d55f806f2b7d6bf98ea9963fb
andreprog02 <98527921+andreprog02@users.noreply.github.com>
2022-02-18 09:40:54 -0300

    Update appop.py

--- appop.py
+++ appop.py
@@ -365,6 +365,6 @@ if imp:
     
    
 if imp:
-    base64_pdf = base64.b64encode(pdf_file.read()).decode('utf-8')
+    base64_pdf = base64.b64encode(cnv.read()).decode('utf-8')
 pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">' 
 st.markdown(pdf_display, unsafe_allow_html=True)
